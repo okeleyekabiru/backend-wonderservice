f922438f30c5f1fc5bb09233cd3c3167cc949dd0
okeleye kabiru <54416255+okeleyekabiru@users.noreply.github.com>
2020-07-02 00:00:14 +0100

    rearrange middleware

--- backend-wonderservice.API/Startup.cs
+++ backend-wonderservice.API/Startup.cs
@@ -52,7 +52,7 @@ namespace backend_wonderservice.API
             {
                 opt.AddPolicy("corsPolicy", builder =>
                 {
-                    builder.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:4200").Build();
+                    builder.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("https://wonderservice.netlify.app").Build();
                 });
             });
             services.AddHangfire(configuration => configuration
@@ -145,12 +145,13 @@ namespace backend_wonderservice.API
                 c.InjectStylesheet("/swagger-ui/custom.css");
 
             });
+            app.UseStaticFiles();
+            app.UseHangfireDashboard();
             app.UseRouting();
             app.UseCors("corsPolicy");
             app.UseAuthentication();
             app.UseAuthorization();
-            app.UseStaticFiles();
-            app.UseHangfireDashboard();
+            
             Hangfire.BackgroundJob.Enqueue(() => Console.Write("running"));
             new BackgroundJob.BackgroundJob();
 
